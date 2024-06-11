fa368d3c87a3db4d5f950a43219e3df74fc18bf6
Willy Renzo <willyrenzodemelo@hotmail.com>
2024-06-10 23:11:41 -0300

    learning command pattern

---
+++ patterns/command/index.js
@@ -0,0 +1,33 @@
+class OrderManager {
+    constructor() {
+      this.orders = [];
+    }
+  
+    execute(command, ...args) {
+      return command.execute(this.orders, ...args);
+    }
+  }
+
+  class Command {
+    constructor(execute) {
+      this.execute = execute;
+    }
+  }
+  
+  function PlaceOrderCommand(order, id) {
+    return new Command((orders) => {
+      orders.push(id);
+      return `You have successfully ordered ${order} (${id})`;
+    });
+  }
+  
+  function CancelOrderCommand(id) {
+    return new Command((orders) => {
+      orders = orders.filter((order) => order.id !== id);
+      return `You have canceled your order ${id}`;
+    });
+  }
+  
+  function TrackOrderCommand(id) {
+    return new Command(() => `Your order ${id} will arrive in 20 minutes.`);
+  }
